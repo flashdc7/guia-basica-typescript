50eb1a07be055bd12a0d8ba0773aa40cf47a3f64
flashdc7 <jrda8303@gmail.com>
2024-10-08 11:19:42 -0600

    NameSpaces

--- bases/main.js
+++ bases/main.js
@@ -1,52 +1,11 @@
 "use strict";
-(() => {
-    let flash = {
-        name: 'Barry Allen',
-        age: 24,
-        powers: ['súper velocidad', 'Viajaer en el tiempo']
+var Validations;
+(function (Validations) {
+    const validateText = (text) => {
+        return (text.length > 3) ? true : false;
     };
-    let superman = {
-        name: 'Clark Kent',
-        powers: ['súper fuerza']
+    const validateDate = (myDate) => {
+        return (isNaN(myDate.valueOf())) ? false : true;
     };
-    console.log(superman);
-})();
-(() => {
-    class Mutante {
-        constructor(age, name, realName) {
-            this.age = age;
-            this.name = name;
-            this.realName = realName;
-        }
-        mutantPower(id) {
-            return this.name;
-        }
-    }
-})();
-(() => {
-    const client = {
-        name: 'ricardo',
-        age: 41,
-        address: {
-            id: 123,
-            zip: 'KY2 ZUM',
-            city: 'Ottawa'
-        }
-    };
-    const client2 = {
-        name: 'irina',
-        age: 45,
-        address: {
-            id: 456,
-            zip: 'KZ4 ZUM',
-            city: 'Toronto'
-        }
-    };
-})();
-(() => {
-    let addTwoNumbers;
-    addTwoNumbers = (a, b) => {
-        return 10;
-    };
-})();
+})(Validations || (Validations = {}));
 //# sourceMappingURL=main.js.map
---
+++ ts-node/src/index.ts
@@ -0,0 +1,15 @@
+// import { Hero as SuperHero } from "./classes/Hero";
+import * as SuperHero from "./classes/Hero";
+// import powersDefault, { Power } from "./data/powers";
+
+
+
+const ironman:SuperHero.Hero= new SuperHero.Hero('Ironman', 1, 55);
+console.log(ironman);
+console.log(ironman.power);
+
+
+
+// console.log(powersDefault);
+
+
